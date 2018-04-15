ef769b2d457a7751ba2fe6f612de30821e3ebcf9
WangZhen <wangzhenjjcn@gmail.com>
2018-04-16 00:57:33 +0800

    ADD

--- get_poi sz.py
+++ get_poi sz.py
@@ -12,13 +12,14 @@ for lines in readed_file:
         data = lines.strip("\n")
         poidatas[data]="readed"
 pagenum=0
-hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+hdr = {
+       'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
+       'Accept-Encoding': 'deflate',
        'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-
+       'Connection': 'keep-alive'
+       }
  
 
 try:
